ef3edf9ac04cadbd59a858614d0cb952d8d7a4f2
piet puk <p.puk@gmail.com>
2016-10-26 12:50:47 +0200

    De eerste versie van gui

---
+++ gui.py
@@ -0,0 +1,54 @@
+from tkinter import *
+
+def label_test():
+    label_test = Label(app, text="De label_test")  # label
+    label_test.grid()  # puts label on screen
+
+#creat window
+root = Tk()
+
+#modify root window
+root.title ("simple GUI")#label
+root.geometry("1000x600")# size
+
+#labels
+app = Frame(root)
+app.grid()#puts app in screen
+
+label = Label(app, text = "De label") #label
+label.grid() #puts label on screen
+
+#buttons
+button1 = Button(text = "Buton", width=10, command=label_test)
+button1.place(x=890, y=0)
+
+button2 = Button(text = "Buton2", width=10)
+button2.place(x=890, y=30)
+
+#grafiek
+
+canvas = Canvas(root, width=800, height=500, bg='white')
+canvas.place(x=10, y=30)
+
+canvas.create_line(50, 400, 750, 400, width=2)  # x-axis    (afstand rand, begin, lengte, einde)
+canvas.create_line(50, 400, 50, 50, width=2)  # y-axis  (afstand rand onder , lengte lijn ,boven links  , lengte van af boven)
+
+
+# x-axis
+for i in range(15):
+    x = 50 + (i * 50)
+    canvas.create_line(x, 400, x, 50, width=1, dash=(2, 5))
+    canvas.create_text(x, 410, text='%d' % (10 * i), anchor=N)
+
+# y-axis
+for i in range(8):
+    y = 400 - (i * 50)
+    canvas.create_line(50, y, 750, y, width=1, dash=(2, 5))
+    canvas.create_text(40, y, text='%d' % (10 * i), anchor=E)
+
+
+#kick off event loop
+
+root.mainloop()
+
+

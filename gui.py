76e65d9121671e0e28bf96ad67e647c888a93af7
piet puk <p.puk@gmail.com>
2016-11-14 21:06:25 +0100

    open en dicht en status

--- gui.py
+++ gui.py
@@ -1,8 +1,9 @@
 from tkinter import *
-from random import randint
+from tkinter import messagebox
 import serial
 
-serialArduino = serial.Serial('COM4', 9600)
+
+serialArduino = serial.Serial('COM3', 9600)
 
 #create window
 root = Tk()
@@ -14,32 +15,101 @@ root.geometry("1000x600")# size
 label = Label( text = "Status:") #label
 label.place(x=50, y=5) #puts label on screen
 
+#variable
+y2 = 0
+var1 = getValueLicht()
+var2 = 12
+uur = 0
+Y2tijdelijk = 400
+knipperTemp = True
+grafiek = True
+yAsVar = 150
+yAsBerekenen = 0.33
+huidige_ondergrens = 600
+huidige_bovengrens = 800
+huidige_bovengrenst = 22
+huidige_ondergrenst = 20
+saveValueLicht = 0
+saveValueTemp = 0
+statusinput = 1
 
 def getValueLicht():
     valueRead = serialArduino.readline()
     if 'L' in str(valueRead):
+        value = getValueOfArduino()
+        if value is None:
+            global saveValueLicht
+            print('Save Value word geprint',saveValueLicht )
+            return saveValueLicht
+        else:
+            global saveValueLicht
+            saveValueLicht = value
+            print('value return',value )
+            return saveValueLicht
+    elif 'T' in str(valueRead):
+        value = getValueOfArduino()
+        print(value,'geen licht')
+        saveValueLicht = 0
+        return 0
+    elif 'S' in str(valueRead):
         print(str(valueRead))
         value = getValueOfArduino()
-        return value
-    elif 'T' in str(valueRead):
-        print(str(valueRead))
-        print('geen licht')
-        return 0
+        if value == 0:
+            Status(1)
+        elif value == 1:
+            Status(3)
+        else:
+            print("pizza")
+        print (value,"dit is geen temp maar een s return saveValueLicht =", saveValueLicht)
+        global saveValueLicht
+        return saveValueLicht
+    elif 'A':
+        Status(2)
+        value = getValueOfArduino()
+        if value is None:
+            return value
+        else:
+            return saveValueLicht
     else:
-        return 0;
+        print('check getValueLicht')
 
 def getValueTemp():
     valueRead = serialArduino.readline()
     if 'L' in str(valueRead):
         print(str(valueRead))
-        print ("dit is geen temp")
+        value = getValueOfArduino()
+        print (value,"dit is geen temp")
+        saveValueTemp = 0
         return 0
+
     elif 'T' in str(valueRead):
         print(str(valueRead))
         value = getValueOfArduino()
-        return value
+        print(value, 'value')
+        if value is None:
+            global saveValueTemp
+            print('Save Value return',saveValueTemp )
+            return saveValueTemp
+        else:
+            global saveValueTemp
+            saveValueTemp = value
+            print('value return',value )
+            return value
+    elif 'S' in str(valueRead):
+        print(str(valueRead))
+        value = getValueOfArduino()
+        if value == 0:
+            Status(1)
+        elif value == 1:
+            Status(3)
+        print (value,"dit is geen temp maar een s return saveValueTemp =", saveValueTemp)
+        global saveValueTemp
+        return  saveValueTemp
+    elif 'A':
+        Status(2)
+        return saveValueTemp
     else:
-        return 0;
+        print('check getValueTemp')
 
 def getValueOfArduino():
     valueRead = serialArduino.readline()
@@ -48,16 +118,7 @@ def getValueOfArduino():
     return valueInInt
 
 
-#variable
-y2 = 0
-var1 = getValue()
-var2 = 12
-uur = 0
-Y2tijdelijk = 400
-knipperTemp = True
-grafiek = True
-yAsVar = 150
-yAsBerekenen = 0.33
+
 
 #grafiek
 
@@ -123,6 +184,72 @@ def newLichtGrafiek():
     yAsVarBer = 0.33
     grafiek = True
 
+def set_open():
+    open =("I"+1)
+    print(open)
+    serialArduino.write(open.encode())
+
+def set_dicht():
+    dicht =("I"+0)
+    print(dicht)
+    serialArduino.write(dicht.encode())
+
+def set_onder():
+    if len(txt_onder.get()) != 0:
+        ondergrens = int(txt_onder.get())
+        if ondergrens < huidige_bovengrens:
+            global huidige_ondergrens
+            huidige_ondergrens = ondergrens
+            ondergrens_tosend =("O"+str(ondergrens))
+            print(ondergrens_tosend)
+            serialArduino.write(ondergrens_tosend.encode())
+        else:
+            messagebox.showerror('Input error', 'Ondergrens ligt boven de bovengrens')
+    else:
+        messagebox.showerror('Input error', 'Voer een geldige waarde in')
+
+def set_boven():
+    if len(txt_boven.get()) != 0:
+        bovengrens = int(txt_boven.get())
+        if bovengrens > huidige_ondergrens:
+            global huidige_bovengrens
+            huidige_bovengrens = bovengrens
+            bovengrens_tosend =("B"+str(bovengrens))
+            print(bovengrens_tosend)
+            serialArduino.write(bovengrens_tosend.encode())
+        else:
+            messagebox.showerror('Input error', 'Bovengrens ligt onder de ondergrens')
+    else:
+        messagebox.showerror('Input error', 'Voer een geldige waarde in')
+
+def set_ondert():
+    if len(txt_onder.get()) != 0:
+        ondergrens = int(txt_ondert.get())
+        if ondergrens < huidige_bovengrenst:
+            global huidige_ondergrenst
+            huidige_ondergrenst = ondergrens
+            ondergrens_tosend = ("K" + str(ondergrens))
+            print(ondergrens_tosend)
+            serialArduino.write(ondergrens_tosend.encode())
+        else:
+            messagebox.showerror('Input error', 'Ondergrens ligt boven de bovengrens')
+    else:
+        messagebox.showerror('Input error', 'Voer een geldige waarde in')
+
+def set_bovent():
+    if len(txt_boven.get()) != 0:
+        bovengrens = int(txt_bovent.get())
+        if bovengrens > huidige_ondergrenst:
+            global huidige_bovengrenst
+            huidige_bovengrenst = bovengrens
+            bovengrens_tosend = ("H" + str(bovengrens))
+            print(bovengrens_tosend)
+            serialArduino.write(bovengrens_tosend.encode())
+        else:
+            messagebox.showerror('Input error', 'Bovengrens ligt onder de ondergrens')
+    else:
+        messagebox.showerror('Input error', 'Voer een geldige waarde in')
+
 #lijn functie
 def lijn():
     global uur,y2 ,var1, Y2tijdelijk, grafiek
@@ -163,7 +290,6 @@ def lijn2():
     y2 = 400 - (var2 * 10)  # lengte lijn 400 ==0
     Y2tijdelijk = y2
     canvas.create_line(x1, y1, x2, y2, fill='blue', tags='temp')
-    print(uur, x1, y1, x2, y2, var2)
     uur += 1
     var2 = getValueTemp()
     if grafiek == False:
@@ -183,38 +309,18 @@ def led(xTemp, collor,reload,functie):
     if reload == True:
         canvas2.after(300, functie)
 
-def knipper():
-    global knipperTemp
-    led(100,'white',False,knipper)  # eerste led
-    led(180,'white',False,knipper)  # derde led
-    print(knipperTemp)
-    # tweed led yellow knipperen
-    if knipperTemp == True:
-        led(140,'yellow',True,knipper)
-        knipperTemp = False
-    else:
-        led(140,'white',True,knipper)
-        knipperTemp = True
-
-def groen():
-    led(100, 'green', True, groen) # eerste led
-    led(140,'white',False,groen)  # tweede led
-    led(180,'white',False,groen)  # derde led
-def rood():
-    led(100, 'white', False, rood) # eerste led
-    led(140,'white',False,rood)  # tweede led
-    led(180,'red',True,rood)  # derde led
-
 #geeft de status waar het scherm zich in bevind
 def Status(status):
     if status == 1:
-        groen()
+        tekst = "dicht"
     elif status == 2:
-        knipper()
+        pass
     elif status == 3:
-        rood()
+        tekst = "open"
     else:
         print('Dit gaat fout check status')
+    label = Label(text=tekst)  # label
+    label.place(x=100, y=5)
 
 # label Boven status
 label = Label(text="Status:")  # label
@@ -224,26 +330,56 @@ label.place(x=50, y=5)  # puts label on screen
 button(840, 30, 'Temperatuur grafiek',newTempGrafiek)
 button(840, 70, 'Licht grafiek',newLichtGrafiek)
 
-# label Configiratie tempratuur
+# label Configiratie scherm
 label = Label( text = "Configuratie: Scherm") #label
 label.place(x=840, y=120) #puts label on screen
 
-buttonKlein(840,145, 'Open',label)
-buttonKlein(920,145, 'Dicht',label)
+buttonKlein(840,145, 'Open',set_open)
+buttonKlein(920,145, 'Dicht',set_dicht)
 
-# label Configiratie licht
-label = Label( text = "Configuratie: Tempratuur") #label
-label.place(x=840, y=190) #puts label on screen
+# label Configiratie temperatuur
+label = Label( text = "Configuratie: Temperatuur") #label
+label.place(x=840, y=195) #puts label on screen
 
-buttonKlein(840,215, 'Plus',label)
-buttonKlein(920,215, 'Min',label)
+#input voor de ondergrens van de temperatuur
+label_ondert = Label(text="Ondergrens:")
+label_ondert.place(x=820,y=220)
+txt_ondert = Entry(root)
+txt_ondert.insert(0,huidige_ondergrenst)
+txt_ondert.place(x=820,y=240)
+btnSetMaxMint = Button(root, text="Set", command=set_ondert)
+btnSetMaxMint.place(x=960, y=234)
+
+#input voor de bovengrens van de temperatuur
+label_bovent = Label(text="Bovengrens:")
+label_bovent.place(x=820,y=260)
+txt_bovent = Entry(root)
+txt_bovent.insert(0,huidige_bovengrenst)
+txt_bovent.place(x=820, y=280)
+btnSetMaxMint = Button(root, text="Set", command=set_bovent)
+btnSetMaxMint.place(x=960, y=276)
 
 # label Configiratie licht
 label = Label( text = "Configuratie: Licht") #label
-label.place(x=840, y=260) #puts label on screen
+label.place(x=840, y=315) #puts label on screen
 
-buttonKlein(840,285, 'Plus',label)
-buttonKlein(920,285, 'Min',label)
+#input voor de ondergrens van het licht
+label_onder = Label(text="Ondergrens:")
+label_onder.place(x=820,y=335)
+txt_onder = Entry(root)
+txt_onder.insert(0,huidige_ondergrens)
+txt_onder.place(x=820,y=355)
+btnSetMaxMin = Button(root, text="Set", command=set_onder)
+btnSetMaxMin.place(x=960, y=351)
+
+#input voor de bovengrens van het licht
+label_boven = Label(text="Bovengrens:")
+label_boven.place(x=820,y=375)
+txt_boven = Entry(root)
+txt_boven.insert(0,huidige_bovengrens)
+txt_boven.place(x=820, y=395)
+btnSetMaxMin = Button(root, text="Set", command=set_boven)
+btnSetMaxMin.place(x=960, y=391)
 
 #kick off event loop
 Status(1)
